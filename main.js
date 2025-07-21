3dd5dd5919375ad9f422e507f3f3da500c3c741d
MountainChickadee <osmo13@gmail.com>
2025-07-21 13:38:16 -0600

    basic arithmatic functions added, roughed out html

---
+++ main.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+function operate(a, b, c) {
+    switch (c) {
+        case 1:
+            return add(a, b);
+        case 2:
+            return subtract(a, b);
+        case 3:
+            return multiply(a, b);
+        case 4:
+            return divide(a, b);
+    }
+}
+
+let a = 2;
+let b = 3;
+let operator = 1;
+
+console.log(operate(a, b, operator));
