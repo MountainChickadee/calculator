1b714321fb40fbfdaeeddb8720bda8745cfa916b
MountainChickadee <osmo13@gmail.com>
2025-07-22 11:37:29 -0600

    have click event listener working correctly

--- main.js
+++ main.js
@@ -27,8 +27,26 @@ function operate(a, b, c) {
     }
 }
 
+function clickHandler(buttonID) {
+    console.log(buttonID);
+}
+
+
+
 let a = 2;
 let b = 3;
 let op = 1;
+
+// runs the display output
+const display = document.querySelector(".display");
+let displayString = '0';
+display.textContent = displayString;
+
+// gets node list of all buttons on the page
+const buttons = document.querySelectorAll("button");
+// sets uo event listeners on each button
+buttons.forEach((button) => {
+    button.addEventListener("click",() => clickHandler(button.id));
+})
 
 console.log(operate(a, b, op));
